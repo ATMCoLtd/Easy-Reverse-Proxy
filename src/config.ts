6a047844b4fa42948cfeb733504be876e0fb7bfa
ATMCoLtd <102722040+ATMCoLtd@users.noreply.github.com>
2025-02-08 22:21:35 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,6 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 export let PROXIES: Proxy[] = [
   {
         domain: "pronet.runtimutd.eu.org",
-        url: "https://whoer.net/",
+        url: "https://github.com/",
   }
 ];
