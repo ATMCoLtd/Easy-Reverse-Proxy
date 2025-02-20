c072436974802323140477ec6754c76ed9779eae
ATMCoLtd <102722040+ATMCoLtd@users.noreply.github.com>
2025-02-20 21:54:58 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,25 +6,7 @@ export let PORT = 377; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-let template: eight = {
-    async response (option) {
-        let bodyString = option.body.toString();
-        bodyString = bodyString.replaceAll('津公网安备 12010302002427号', '');
-        bodyString = bodyString.replaceAll('874236技术分享站', '874236镜像站');
-        bodyString = bodyString.replaceAll('津ICP备2023004796号', '');
-        bodyString = bodyString.replaceAll('津ICP备2023004796号-1', '');
-        option.body = Buffer.from(bodyString);
-        return option;
-    }
-};
-let template: Google = {
-    async response (option) {
-        let bodyString = option.body.toString();
-        bodyString = bodyString.replaceAll('https://www.google.com/', '/');
-        option.body = Buffer.from(bodyString);
-        return option;
-    }
-};
+
 
 export let PROXIES: Proxy[] = [
  {
