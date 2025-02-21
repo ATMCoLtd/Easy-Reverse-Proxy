05aaa776fe2d0b5faaa0ca143870b3a0c2685360
ATMCoLtd <102722040+ATMCoLtd@users.noreply.github.com>
2025-02-21 14:24:26 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export let PROXIES: Proxy[] = [
  },
  {
         domain: "test.runtimutd.eu.org",
-        url: "https://telegram.org",
+        url: "https://td.telegram.org",
         enable :true
 },
 {
