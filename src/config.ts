785f01efcf42cf42b87a047fdeb4a68a2df868bd
ATMCoLtd <102722040+ATMCoLtd@users.noreply.github.com>
2025-03-07 07:41:34 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export let PROXIES: Proxy[] = [
  },
  {
         domain: "test.runtimutd.eu.org",
-        url: "https://grok.com",
+        url: "https://basic.smartedu.cn",
         enable :true
 },
 {
